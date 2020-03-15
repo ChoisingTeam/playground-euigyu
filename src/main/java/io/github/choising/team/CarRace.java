bad4bb3a9d2e680a7bed93fde49ea77c3c6fc41b
euigyu <afterscool926@gmail.com>
2020-03-15 21:07:50 +0900

    check

--- src/main/java/io/github/choising/team/CarRace.java
+++ src/main/java/io/github/choising/team/CarRace.java
@@ -6,30 +6,37 @@ import java.io.*;
 
 
 public class CarRace {
+    public CarRace(){
 
-    public void race(int count) {
+    }
+
+    public void inputRand(int count) {
         for (int i = 0; i < count; i++) {
             int rand = (int) (Math.random() * 10);
-            if (rand >= 4) {
-                System.out.print("-");
-            }
+            race(rand);
         }
         System.out.println();
     }
 
+    public void race(int num){
+        if(num>=4){
+            System.out.print("-");
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("자동차 대수는 몇 대 인가요?");
         int car = sc.nextInt();
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int count = sc.nextInt();
-
         System.out.println();
+
         System.out.println("실행 결과");
         for(int i=0;i<car;i++){
             CarRace car_num = new CarRace();
-            car_num.race(count);
+            car_num.inputRand(count);
         }
 
     }
